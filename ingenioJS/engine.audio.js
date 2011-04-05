8da8debe522b9b3553affae497c5000f7f79d13b
Christoph Martens <christoph.martens@ymail.com>
2011-04-05 18:37:29 +0200

    Bunches of improvements. audio engine works for most browsers now. Apparently, it works smooth on Android - but really slow on iOS due to codec and currentTime-shifting delay for playback. Needs to be fixed or workaround-ed

--- ingenioJS/engine.audio.js
+++ ingenioJS/engine.audio.js
@@ -5,7 +5,7 @@
  * @returns {Object} controller plugin instance
  * @todo Improve audio engine with multi-channel and multi-stream functionalities; Improve also a flash-using fallback
  */
-ingenioJS.engine.audio = function(settings){
+ingenioJS.engine.audio = function(settings, callback){
 
 	// defaults
 	/*
@@ -50,6 +50,9 @@ ingenioJS.engine.audio = function(settings){
 	// initialize the audio
 	this.init(this.settings);
 
+	// callback function
+	callback && callback(this);
+
 	// called for multiple streams
 	if(this.settings.length){
 		return this.streams;
@@ -62,45 +65,197 @@ ingenioJS.engine.audio = function(settings){
 
 ingenioJS.engine.audio.prototype = {
 
+	_createChannel: function(stream){
+
+		var newStream = {
+			context: stream.context.cloneNode(true),
+			type: stream.type,
+			jumpmarks: stream.jumpmarks
+		};
+
+		// set flag that this is just a clone =)
+		newStream.isChannel = true;
+
+		// attach the per-stream functionality
+		newStream.play = this.play;
+		newStream.stop = this.stop;
+		newStream.pause = this.pause;
+		newStream.resume = this.resume;
+
+		// streams have a link to the queue to influence _loop() behaviour
+		newStream._queue = this._queue;
+
+		return newStream;
+	},
+
 	_detectFeatures: function(){
 
 		this.features = {};
 
 		var audio = window.Audio && new Audio();
 		if(audio && audio.canPlayType){
-			this.features['mp3'] = (audio.canPlayType('audio/mpeg') != "");
-			this.features['ogg'] = (audio.canPlayType('audio/ogg; codecs="vorbis"') != "");
+
+			// this is the list we will walk through to check codec support
+			var mimeList = [
+				// e = extension, m = mime type
+				{ e: '3gp', m: 'audio/3gpp' },
+				// { e: 'avi', m: 'video/x-msvideo' }, // avi container allows pretty everything, impossible to detect -.-
+				{ e: 'aac', m: [ 'audio/aac', 'audio/aacp' ] },
+				{ e: 'amr', m: 'audio/amr' },
+				{ e: 'm4a', m: [ 'audio/mp4', 'audio/mpeg4', 'audio/mp4a-latm' ] },
+				{ e: 'mp3', m: [ 'audio/mp3', 'audio/mpeg' ] }, // mpeg was name for mp2 and mp3! avi container was mp4/m4a
+				{ e: 'mpga', m: [ 'audio/mpeg', 'video/mpeg' ] },
+				{ e: 'mp4', m: 'video/mp4' },
+				{ e: 'ogg', m: [ 'application/ogg', 'audio/ogg', 'audio/ogg; codecs="theora, vorbis"', 'video/ogg', 'video/ogg; codecs="theora, vorbis"' ] },
+				{ e: 'wav', m: [ 'audio/wave', 'audio/wav', 'audio/x-wav', 'audio/x-pn-wav' ] },
+				{ e: 'webm', m: [ 'audio/webm', 'video/webm' ] }
+			];
+
+			var mime = undefined,
+				extension = undefined;
+			for(var m=0; m<mimeList.length; m++){
+				extension = mimeList[m].e;
+
+				if(mimeList[m].m.length && typeof mimeList[m].m == 'object'){
+					// walk through array
+					for(var mm=0; mm<mimeList[m].m.length; mm++){
+						mime = mimeList[m].m[mm];
+
+						if(audio.canPlayType(mime) != ""){
+							this.features[extension] = true;
+							break; // we found a supported codec for extension, so skip redundant checks
+						}
+					}
+				}else{
+					mime = mimeList[m].m;
+
+					if(audio.canPlayType(mime) != ""){
+						this.features[extension] = true;
+					}
+				}
+
+				// prevent iteration mistakes
+				mime = undefined;
+				extension = undefined;
+			}
 
 			// set a flag if browser supports our audio api which is the prefered one
-			this.features['html5'] = this.features['mp3'] || this.features['ogg'];
+			this.features['html5'] = this.features['aac'] || this.features['mp3'] || this.features['ogg'] || this.features['webm'] || false;
+
+			// detect via audio api extension, default is 4
+/*
+			var self = this;
+			audio.addEventListener('loadedmetadata', function(){
+				// we can only here detect whether audio api extension is accessible.
+				// ... due to those fucking DOM Exceptions. Dude, that just sucks hard.
+				self.features['channels'] = (this.channels || this.mozChannels || this.webkitChannels) || 4;
+			}, true);
+*/
+
+			// no on-demand detection using audio api extension conceptionated by Mozilla. see above.
+			this.features['channels'] = 4;
+
+			// check if the browser supports the volume property
+			audio.volume=0.1;
+			this.features['volume'] = !!audio.volume.toString().match(/^0\.1/);
+
+			// hacky, but there's no method to detect that =/
+			if(navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)){
+				this.features['channels'] = 1;
+			}
 
 		}
 
-
 		// TODO: This needs to be detected somehow.
-		this.features['embed'] = this.features['mid'] = this.features['midi'] = true;
+		// this.features['embed'] = this.features['mid'] = this.features['midi'] = true;
 
-/*
-	    var audio = window.Audio && new Audio();
-	    if (audio && audio.canPlayType) {
-	        c.canPlayMp3 = audio.canPlayType('audio/mpeg') != "";
-	        c.canPlayOgg = audio.canPlayType('audio/ogg; codecs="vorbis"') != "";
-		    c.canPlay = c.canPlayMp3 || c.canPlayOgg;
-	        c.channels = 4;
-	        c.volumeControl = true;
-	        c.canPlayAnytime = true;
+	},
 
-			// not the best solution and hacky, but limited channel
-	        if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) {
-	            c.channels = 1;
-		        c.volumeControl = false;
-		        c.canPlayAnytime = false;
-	        }
-		    if (c.canPlay) {
-			    c.preferedType = c.canPlayOgg ? "ogg" : "mp3";
-		    }
-	    }
+	_initLoop: function(){
+
+		this._initialized = true;
+		this._loopDelay = 100;
+
+		if(this.streams.length){
+
+			// automatically create streams for missing channels
+			var missingChannels = (this.features.channels - this.streams.length);
+			if(this.streams.length < missingChannels){
+				for(var i=0; i<missingChannels; i++){
+					this.streams.push(this._createChannel(this.streams[0]));
+				}
+			}
+
+			// TODO: find a dynamic way or set a flag if an html5 stream exists
+			if(this.streams[0].type == 'html5'){
+				// only html5 audio api requires sound loop and correction.
+				// required due to slow cpu and audio api implementations
+				var self = this;
+				self._loopId = window.setInterval(function(){
+					self._loop.apply(self);
+				}, self._loopDelay);
+			}
+		}
+
+	},
+
+	_loop: function(){
+
+		var stream = undefined;
+
+		// walk through playing streams and stop or restart them if they are looped.
+		for(var s=0; s<this.streams.length; s++){
+
+			stream = this.streams[s];
+
+			// hacky, but it's like that.
+			stream._queue = this._queue;
+
+			if(stream._playing){
+				// stream seems to have finished playback
+				if(stream.context.currentTime >= stream._playing.end){
+
+					// restart stream
+					if(stream._playing.loop === true){
+						var newPointer = stream._playing.start;
+						this.streams[s].stop();
+						this.streams[s].play(newPointer);
+					// stop stream
+					}else{
+						this.streams[s].stop();
+					}
+
+				// start stream, try & error due to dom exceptions for currentTime access
+				}else if(stream.context.currentTime < stream._playing.start){
+					this.streams[s].context.currentTime = stream._playing.start;
+
+					// TODO: This is required for iOS due to heavily slow implementation.
+					// Needs a fix or a different solution. above should check if !stream.locked
+
+/*					try{
+						this.streams[s].context.currentTime = stream._playing.start;
+						this.streams[s].locked = true;
+					}catch(e){
+						this.streams[s].tries = this.streams[s].tries ? (this.streams[s].tries+1) : 1;
+						console.log(this.streams[s].tries); // randomly between 1000ms and 2500ms (only iOS!) are required for initializing the playback readiness
+					}
 */
+				}
+
+			// use a free channel to play the file
+			}else if(!stream._playing && stream.isChannel && this._queue.length){
+
+				var mark = this._queue[0].mark,
+					loop = this._queue[0].loop;
+
+				// clean first queue entry
+				this._queue = this._queue.splice(1);
+
+				stream.play(mark, loop);
+
+			}
+
+		}
 
 	},
 
@@ -113,6 +268,7 @@ ingenioJS.engine.audio.prototype = {
 
 		if(!this.streams){
 			this.streams = [];
+			this._queue = [];
 		}
 
 		if(!settings.length){
@@ -130,73 +286,52 @@ ingenioJS.engine.audio.prototype = {
 						ext = ext[ext.length - 1];
 
 					if(ext && this.features[ext] === true){
-						stream.type = ext;
 						stream.resource = set.resources[r];
-					}
-
-					// hacky... but these are the prefered formats
-					if(stream.resource && stream.type == ('mp3' || 'ogg')){
 						break;
 					}
 				}
+
+			// stream has no playable media type
 			}else{
-				// stream has no playable media type, so flag it to false
-				stream = false;
 				continue; // go to next stream settings
 			}
 
-			if(stream && stream.type){
-				switch(stream.type){
-					case "mid": case "midi":
-						set.type = 'embed';
-					break;
-					case "mp3": case "ogg":
-						set.type = 'html5';
-					break;
-					default: // default will try to use a browser plugin!
-						set.type = 'embed';
-					break;
-				}
+			if(stream && stream.resource){
 
-				if(set.type == 'html5' && this.features[set.type] === true){
+				if(this.features['html5'] === true){
 
 					// cache the type for later usage
-					stream.type = set.type;
+					stream.type = 'html5';
 
-					stream.context = document.createElement('audio');
+					stream.context = new Audio();
 					stream.context.id = 'ingenioJS-audio-'+(this.streams.length + 1);
 					stream.context.src = stream.resource;
-					stream.context.setAttribute('class', 'audio-stream');
+
+					// old WebKit
+					stream.context.autobuffer = true;
+					// new WebKit
+					stream.context.preload = true;
+
+					if(set.autoplay){
+						stream.context.autoplay = true;
+					}
+
+					stream.context.preload = 'auto';
 
 					// DEBUGGING
 					// stream.context.setAttribute('controls', 'true');
 
-					if(set.autoplay){
-						stream.context.setAttribute('autoplay', 'true');
-					}
-
 					// append the audio stream now
 					document.getElementsByTagName('body')[0].appendChild(stream.context);
 
-				}else if(set.type == 'embed' && this.features[set.type] === true){
+				}else if(this.features['embed'] === true){
 
 					stream.context = document.createElement('embed');
 					stream.context.id = 'ingenioJS-audio-'+(this.streams.length + 1);
 					stream.context.src = stream.resource;
 					stream.context.setAttribute('class', 'audio-stream');
-
-					var mime = false;
-
-					// set the corresponding mime-type
-					switch(stream.type){
-						case "mid":
-						case "midi":
-							mime = 'audio/midi';
-						break;
-					}
-
-					stream.context.setAttribute('type', (mime ? mime : 'audio/*' ));
-
+//					stream.context.setAttribute('type', stream.type );
+//					TODO: lookup mime type
 					stream.context.setAttribute('hidden' , 'true');
 
 					if(set.autoplay){
@@ -217,20 +352,23 @@ ingenioJS.engine.audio.prototype = {
 					stream.jumpmarks = set.jumpmarks;
 				}
 
-				// cache the type for later usage
-				stream.type = set.type;
-
 				// attach the per-stream functionality
 				stream.play = this.play;
 				stream.stop = this.stop;
-				stream.start = this.resume;
 				stream.pause = this.pause;
 				stream.resume = this.resume;
+
+				// streams have a link to the queue to influence _loop() behaviour
+				stream._queue = this._queue;
 
 				this.streams.push(stream);
 
 			}
 
+		}
+
+		if(!this._initialized){
+			this._initLoop();			
 		}
 
 	},
@@ -245,6 +383,7 @@ ingenioJS.engine.audio.prototype = {
 
 		var mark = false,
 			loop = false;
+
 		if(arguments[0] !== undefined){
 			mark = arguments[0];
 		}
@@ -252,43 +391,69 @@ ingenioJS.engine.audio.prototype = {
 			loop = arguments[1];
 		}
 
+		// create a queue entry
+		if(this._playing){
+			var queueEntry = { mark: mark, loop: loop, stream: this };
+			this._queue.push(queueEntry);
+			return;
+		}
+
 		// jumpmarks are only available for single-stream usage
 		if(mark && this.jumpmarks && this.jumpmarks[mark]){
-			if(this.context){
-				var jumpTo = this.jumpmarks[mark].start,
-					tmp = [];
+			var jumpTo = this.jumpmarks[mark].start,
+				tmp = [];
 
-				// 1:20:10 -> 80.10
-				if(jumpTo.match(/:/)){
-					tmp = jumpTo.split(':');
-					jumpTo = parseInt(0,10); // integer required!
+			// 1:20:10 -> 80.10
+			if(typeof jumpTo == 'string' && jumpTo.match(/:/)){
+				tmp = jumpTo.split(':');
+				jumpTo = parseInt(0,10); // integer required!
 
-					tmp[0] = parseInt(tmp[0],10) * 60; // minutes
-					tmp[1] = parseInt(tmp[1],10); // seconds
-					tmp[2] = parseInt(tmp[2],10) / 60; // milliseconds (-> format: s.ms)
+				tmp[0] = parseInt(tmp[0],10) * 60; // minutes
+				tmp[1] = parseInt(tmp[1],10); // seconds
+				tmp[2] = parseInt(tmp[2],10) / 60; // milliseconds (-> format: s.ms)
 
-					jumpTo += tmp[0] + tmp[1] + tmp[2];
-				}
+				jumpTo += tmp[0] + tmp[1] + tmp[2];
+			}
 
-				if(this.type == 'html5'){
-					if(!this._playing){
-						this._playing = this.context.play();
+			if(this.context && this.type == 'html5'){
+				if(!this._playing){
+					this.context.play();
+
+					// cache the jumpmark's details for quick access
+					this._playing = this.jumpmarks[mark];
+
+					try{
+						this.context.currentTime = jumpTo;
+						this.isReady = true;
+					}catch(e){
+						this.isReady = false;
 					}
 
-					this.context.currentTime = Math.round(jumpTo);
 				}
 			}
 		}else if(typeof mark == 'number'){
-			if(this.context){
-				if(this.type == 'html5'){
-					if(!this._playing){
-						this._playing = this.context.play();
+			if(this.context && this.type == 'html5'){
+				if(!this._playing){
+					this.context.play();
+
+					// find the right jumpmark
+					for(var jm in this.jumpmarks){
+						if(mark >= this.jumpmarks[jm].start && mark <= this.jumpmarks[jm].end){
+							this._playing = this.jumpmarks[jm];
+							break;
+						}
 					}
 
-					this.context.currentTime = mark;
+					try{
+						this.context.currentTime = mark;
+						this.isReady = true;
+					}catch(e){
+						this.isReady = false;
+					}
 				}
 			}
 		}
+
 	},
 
 	/**
@@ -300,7 +465,7 @@ ingenioJS.engine.audio.prototype = {
 		if(this.type == 'html5'){
 			this.lastPointer = 0; // reset pointer
 			this.context.pause();
-			this._playing = false;
+			this._playing = undefined;
 		}else if(this.type == 'embed'){
 			// TODO: Find out how "most" of the plugin APIs work
 			// Removing the attribute does nothing. That kinda sucks, dude.
@@ -327,18 +492,14 @@ ingenioJS.engine.audio.prototype = {
 	resume: function(){
 
 		if(this.type == 'html5'){
+
 			// only set play position in spritemap if we got a last pointer (set by pause())
-
-console.log(this.lastPointer);
-
 			if(this.lastPointer !== undefined){
 				this.play(this.lastPointer);
 				this.lastPointer = undefined;
 			}else{
 				this.context.play();
 			}
-
-console.log('resume!!!', this.lastPointer);
 
 		}else if(this.type == 'embed'){
 			// TODO: Find out how "most" of the plugin APIs work
